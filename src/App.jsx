ddbff67c90854431118b1f3744bd7c9bccbf89df
HasnainAli313 <aliorakzai407@gmail.com>
2024-10-11 21:42:26 +0500

    initialized states

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Button, TextField, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
+
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState({ name: "", email: "" });
+  const [updateUser, setUpdateUser] = useState({ id: "", name: "", email: "" });
+  const [openAddDialog, setOpenAddDialog] = useState(false);
+  const [openEditDialog, setOpenEditDialog] = useState(false);
+
+  return (
+  )
+}
+
+export default App
